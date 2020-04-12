bd180c1634498a7190a2ff42f23d351f7a422564
baijunjie <bjjjunjie@qq.com>
2020-04-12 17:04:39 +0800

    生成预测模型已预测测试

---
+++ ml/ml/IrisData.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.ML.Data;
+
+namespace ml
+{
+    // <SnippetClassDefinitions>
+    public class IrisData
+    {
+        [LoadColumn(0)]
+        public float SepalLength;
+
+        [LoadColumn(1)]
+        public float SepalWidth;
+
+        [LoadColumn(2)]
+        public float PetalLength;
+
+        [LoadColumn(3)]
+        public float PetalWidth;
+    }
+
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+    
+   
+}
---
+++ ml/ml/TestIrisData.cs
@@ -0,0 +1,14 @@
+namespace ml
+{
+    public class TestIrisData
+    {
+        // <SnippetTestData>
+        internal static readonly IrisData Setosa = new IrisData
+        {
+            SepalLength = 5.1f,
+            SepalWidth = 3.5f,
+            PetalLength = 1.4f,
+            PetalWidth = 0.2f
+        };
+    }
+}
